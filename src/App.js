bef29d706bdefc925327c80f0cb186398f9395f8
Rodika <raryku@yahoo.com>
2023-02-08 21:24:40 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import video from './ocean.mp4';
 
 function App() {
+
+  const [tips, setTips] = useState("");
+
+  const newTips = async () => {
+  const response = await fetch("http://www.boredapi.com/api/activity/");
+  const data = await response.json();
+  setTips(data.activity);
+  };
+  useEffect(() => {
+    newTips();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <video autoPlay muted loop>
+          <source src={video} type="video/mp4"/>
+          </video>
+          <h1>Are you bored?</h1>
+        <h4>Click the button to find a new tip</h4>
+        <p className="par">{tips}</p>
+        <button className="btn" onClick={newTips}>New Tip</button>
+      </div>
     </div>
+  
   );
 }
-
 export default App;
